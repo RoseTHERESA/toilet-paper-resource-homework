a2ab0e5a8bd88d34138d874a28cf0c627f319fea
Rose THERESA <rose.minta.theresa@gmail.com>
2015-06-14 18:48:09 -0700

    add missing routes

--- app.js
+++ app.js
@@ -21,25 +21,73 @@ app.get('/', function(req,res){
 app.get('/toilet_paper', function(req,res){
 	db.ToiletPaper.find({}, function(err, tp){
 		if (err) throw err;
-		res.render("toilet_paper/index", {tp:tp});
-	});
+		res.render("/toilet_paper/index", {tp:tp});
+	})
 });
 
-//NEW
+//NEW -- FORM
 app.get('/toilet_paper/new', function(req,res){
-	res.render("toilet_paper/new");
+	res.render("/toilet_paper/new");
 })
 
-//CREATE
+//CREATE A NEW RECORD
 app.post('/toilet_paper', function(req,res){
 	db.ToiletPaper.create(req.body.tp, function(err){
 		console.log("is tp correct?")
 		res.redirect("/toilet_paper");
-	});
+	})
 });
 
+//SHOW ONE RECORD
+app.get('/toilet_paper/:id', function(req,res){
+	db.ToiletPaper.findById(req.params.id, function(err, tp){
+		if(err){
+			res.render("404");
+		} else {
+			res.render("show", {tp:tp});
+		}
+	})
+});
 
+//EDIT -- DISPLAY FORM
+app.get('/toilet_paper/:id/edit', function(req,res){
+	db.ToiletPaper.findById(req.params.id, function(err, tp){
+		if(err){
+			res.render("404");			
+		} else {
+			res.render('edit', {tp:tp});
+		}
+	})
+});
 
+//EDIT -- MODIFY DATA
+app.put('/toilet_paper/:id', function(req,res){
+	db.ToiletPaper.findByIdAndUpdate(req.params.id, req.body.book, function(err, book){
+		if(err){
+			res.render("404");
+		} else {
+			res.redirect("/toilet_paper");
+		}
+	})
+});
+
+//DELETE ONE RECORD
+app.delete('/toilet_paper/:id', function(req,res){
+	db.ToiletPaper.findByIdAndRemove(req.params.id, function(err, tp){
+		if(err){
+			res.render("404");
+		} else {
+			res.redirect('/toilet_paper');
+		}
+	})
+});
+
+//ERROR WILDCARD
+app.get('*', function(req,res){
+  res.render('404');
+});
+
+//SERVER
 app.listen(3000, function(){
   console.log("Server is listening on port 3000");
 });
